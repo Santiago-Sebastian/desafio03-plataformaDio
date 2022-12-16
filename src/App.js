2c988c225a182bd5a46a788d03ffb62cec3ca612
Santiago Sebastian <santiagosebastian900@gmail.com>
2022-12-16 02:58:54 -0300

    Desafio 03

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Feed } from "./pages/feed";
+import { Cadastro } from "./pages/cadastro";
+import { Home } from './pages/home'
+import { Login } from './pages/login'
+import { GlobalStyle } from './styles/global';
+
+function App() {
+  return (
+    <Router>
+     <GlobalStyle />
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+     </Routes >
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/cadastro/index.jsx
@@ -0,0 +1,75 @@
+import { useNavigate  } from "react-router-dom";
+import { MdEmail, MdLock, MdPerson, MdPhone } from 'react-icons/md'
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { api } from '../../services/api';
+
+import { useForm } from "react-hook-form";
+
+
+import { Container, Title, Column, TitleLogin, SubtitleLogin, Text, LoginText, Row, Wrapper } from './styles';
+
+const Cadastro = () => {
+
+    const navigate = useNavigate()
+
+    const { control, handleSubmit, formState: { errors  } } = useForm({
+        reValidateMode: 'onChange',
+        mode: 'onChange',
+    });
+
+    const onSubmit = async (formData) => {
+        try{
+            const {data} = await api.get(`/users?email=${formData.email}&senha=${formData.senha}`);
+
+            if(data.length && data[0].id){
+                navigate('/feed') 
+                return
+            }
+
+            alert('Usuário ou senha inválido')
+        }catch(e){
+            alert("Erro ao cadastrar")
+        }
+    };
+
+    console.log('errors', errors);
+
+    return (<>
+        <Header />
+        <Container>
+            <Column>
+                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
+                 e entrar mais rápido nas empresas mais desejadas.</Title>
+            </Column>
+            <Column>
+                <Wrapper>
+                <TitleLogin>Comece agora grátis</TitleLogin>
+                <SubtitleLogin>crie sua conta e make the change._</SubtitleLogin>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Input placeholder="Nome completo" leftIcon={<MdPerson />} name="nomecompleto"  control={control} />
+                    {errors.nome && <span>O nome é obrigatório</span>}
+                    <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
+                    {errors.email && <span>E-mail é obrigatório</span>}
+                    <Input type="celular" placeholder="Celular" leftIcon={<MdPhone />}  name="celular" control={control} />
+                    {errors.celular && <span>Celular é obrigatório</span>}
+                    <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" control={control} />
+                    {errors.senha && <span>Senha é obrigatório</span>}
+                    <Button title="CRIAR MINHA CONTA GRÁTIS" variant="secondary" type="submit"/>
+                </form>
+                <Row>
+                    <Text>Ao clicar em "criar minha conta grátis", declaro que aceito as Políticas de Privacidade e os Termos de Uso da DIO.</Text>
+                </Row>
+                <Row>
+                    <LoginText>Já tenho conta. <a href='/login'>Fazer login.</a>
+                    </LoginText>
+
+                </Row>
+                </Wrapper>
+            </Column>
+        </Container>
+    </>)
+}
+
+export { Cadastro }
